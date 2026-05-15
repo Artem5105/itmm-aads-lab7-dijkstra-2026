47c21608959efd9b1e708fdc6669d81a0043d74a
EvgenyPinezhanin <pinezhanin@yandex.ru>
2026-05-15 11:44:09 +0300

    Added lab template

---
+++ src/tdijkstra.cpp
@@ -0,0 +1,27 @@
+// ННГУ, ИИТММ, Курс "Алгоритмы и структуры данных"
+//
+// tdijkstra.cpp
+//
+// Copyright (c) Пинежанин Е.С.
+
+#include <tdijkstra.h>
+
+void TDijkstra::compute(int start) {
+
+}
+
+int TDijkstra::getDistance(int v) const {
+    return 0;
+}
+
+const std::vector<int>& TDijkstra::getAllDistances() const {
+    return std::vector<int>{};
+}
+
+std::vector<int> TDijkstra::getPath(int v) const {
+    return std::vector<int>{};
+}
+
+bool TDijkstra::isReachable(int v) const {
+
+}
---
+++ test/test_tdijkstra.cpp
@@ -0,0 +1,8 @@
+#include "tdijkstra.h"
+
+#include <gtest.h>
+
+TEST(TDijkstra, test)
+{
+  ADD_FAILURE();
+}
